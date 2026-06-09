afa0c9e8a3768734f612f0b0ab6cf9e6f90dec52
Anna Moreno Bonell <morenobo87@gmail.com>
2026-06-09 19:01:23 +0200

    ejercicios_phyton

--- Python BQA/Ejercicios/t1e1peliculas.py
+++ Python BQA/Ejercicios/t1e1peliculas.py
@@ -1,4 +1,4 @@
-# 1. Declaramos las variables con los datos de la película
+
 titulo = "Paris, Texas"
 director = "Wim Wenders"
 ano_lanzamiento = 1984
@@ -7,8 +7,6 @@ duracion_minutos = 145
 tiene_premios = True 
 
 
-# 2. Mostramos la información en la consola
-print("--- DATOS DE MI PELÍCULA FAVORITA ---")
 print("Título:", titulo)
 print("Director:", director)
 print("Año de lanzamiento:", ano_lanzamiento)
@@ -17,12 +15,10 @@ print("Duración en minutos:", duracion_minutos)
 print("¿Tiene premios?:", tiene_premios)
 
 
-# 3. Cambia los valores de las variables a datos en inglés
 titulo = "Paris, Texas (English Release)"
 genero = "Drama / Road Movie"
 
 
-# 4. Vuelve a mostrar la información en la consola para ver los cambios
 print("Title:", titulo)
 print("Director:", director)
 print("Release Year:", ano_lanzamiento)
